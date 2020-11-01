67def3ccfd43c2134c50ce854a712f8be3f77e53
bpeldi2oerkd8 <pl_career2es0f_18baby6@yahoo.co.jp>
2020-11-01 16:47:00 +0900

    SlackのID登録機能の実装

--- routes/slack-id-register.js
+++ routes/slack-id-register.js
@@ -2,9 +2,24 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const User = require('../models/user');
 
 router.get('/', authenticationEnsurer, (req, res, next) => {
   res.render('slack-id-register', { user: req.user });
+});
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  const userId = req.user.id;
+  const userName = req.user.username;
+  const slackId = req.body.slackId;
+
+  User.create({
+    userId: userId,
+    userName: userName,
+    slackId: slackId
+  }).then(() => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
